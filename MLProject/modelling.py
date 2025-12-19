f49ce18f2a08fd4f1da825b0e7dd331a48da6b1a
Rafias01 <rafiasubekti@gmail.com>
2025-12-19 23:04:51 +0700

    perbaikan modelling again

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,20 +2,17 @@ import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
-import joblib
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "bank_dataset_preprocessing.csv")
-
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("Bank Churn - Baseline Model")
 
 print("MLflow Project run aktif")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_PATH = os.path.join(BASE_DIR, "bank_dataset_preprocessing.csv")
 df = pd.read_csv(DATA_PATH)
 
 X = df.drop("Exited", axis=1)
@@ -28,8 +25,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(
+    max_iter=1000,
+    solver="lbfgs"
+)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -39,6 +38,7 @@ f1 = f1_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
 
 mlflow.log_param("model_type", "LogisticRegression")
+mlflow.log_param("solver", "lbfgs")
 mlflow.log_metric("accuracy", acc)
 mlflow.log_metric("f1_score", f1)
 
@@ -51,8 +51,8 @@ with open(cm_path, "w") as f:
     f.write(str(cm))
 mlflow.log_artifact(cm_path)
 
-mlflow.sklearn.log_model(model, artifact_path="model")
+mlflow.sklearn.log_model(model, "model")
 
-print("✅ Training & logging selesai")
+print("Training selesai")
 print("Accuracy:", acc)
 print("F1-score:", f1)
