6eac2865a50fdebafc2f9c0c6cbaa51cbf393cc5
Rafias01 <rafiasubekti@gmail.com>
2025-12-19 23:21:24 +0700

    perbaikan

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -12,43 +12,49 @@ from sklearn.preprocessing import StandardScaler
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "bank_dataset_preprocessing.csv")
 
+RUN_ID = os.environ.get("MLFLOW_RUN_ID")
+if RUN_ID is None:
+    raise RuntimeError("MLFLOW_RUN_ID tidak ditemukan. Script ini harus dijalankan via `mlflow run`.")
+
 mlflow.set_experiment("Bank Churn - Baseline Model")
 
-df = pd.read_csv(DATA_PATH)
+with mlflow.start_run(run_id=RUN_ID):
 
-X = df.drop("Exited", axis=1)
-y = df["Exited"]
+    df = pd.read_csv(DATA_PATH)
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+    X = df.drop("Exited", axis=1)
+    y = df["Exited"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42, stratify=y
-)
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
 
-model = LogisticRegression(max_iter=2000, solver="lbfgs")
-model.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, test_size=0.2, random_state=42, stratify=y
+    )
 
-y_pred = model.predict(X_test)
+    model = LogisticRegression(max_iter=2000, solver="lbfgs")
+    model.fit(X_train, y_train)
 
-acc = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-cm = confusion_matrix(y_test, y_pred)
+    y_pred = model.predict(X_test)
 
-mlflow.log_param("model_type", "LogisticRegression")
-mlflow.log_param("solver", "lbfgs")
-mlflow.log_param("max_iter", 2000)
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
 
-mlflow.log_metric("accuracy", acc)
-mlflow.log_metric("f1_score", f1)
+    mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("solver", "lbfgs")
+    mlflow.log_param("max_iter", 2000)
 
-model_path = os.path.join(BASE_DIR, "logreg_model.pkl")
-joblib.dump(model, model_path)
-mlflow.log_artifact(model_path)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
 
-cm_path = os.path.join(BASE_DIR, "confusion_matrix.txt")
-with open(cm_path, "w") as f:
-    f.write(str(cm))
-mlflow.log_artifact(cm_path)
+    model_path = os.path.join(BASE_DIR, "logreg_model.pkl")
+    joblib.dump(model, model_path)
+    mlflow.log_artifact(model_path)
 
-mlflow.sklearn.log_model(model, artifact_path="model")
+    cm_path = os.path.join(BASE_DIR, "confusion_matrix.txt")
+    with open(cm_path, "w") as f:
+        f.write(str(cm))
+    mlflow.log_artifact(cm_path)
+
+    mlflow.sklearn.log_model(model, artifact_path="model")
